1c69e5feb8bbf9d8bd784d1192abeb71c15bc512
ByeongGyu Jeon <cube9928@naver.com>
2025-01-27 14:32:39 +0900

    feat : 가이드 API 초기세팅 및 상세 조회 API 구현 #2

---
+++ backend/src/main/java/com/tripmarket/domain/guide/controller/GuideController.java
@@ -0,0 +1,63 @@
+package com.tripmarket.domain.guide.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.tripmarket.domain.guide.dto.GuideDto;
+import com.tripmarket.domain.guide.service.GuideService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+
+@RestControllerAdvice
+@RestController
+@RequestMapping("/guides")
+@Tag(name = "GuideController", description = "가이드 컨트롤러")
+public class GuideController {
+	private final GuideService guideService;
+
+	@Autowired
+	public GuideController(GuideService guideService) {
+		this.guideService = guideService;
+	}
+
+	@Operation(summary = "가이드 상세 조회")
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public GuideDto findGuideById(@PathVariable(name = "id") Long id) {
+		return guideService.findById(id);
+	}
+
+	@Operation(summary = "가이드 생성")
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createGuide(@Valid @RequestBody GuideDto guideDto) {
+		guideService.create(guideDto);
+	}
+
+	@Operation(summary = "가이드 리스트 조회")
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public List<GuideDto> findGuides() {
+		return guideService.findAll();
+	}
+
+	@Operation(summary = "가이드 수정")
+	@PatchMapping
+	@ResponseStatus(HttpStatus.OK)
+	public void updateGuide(@Valid @RequestBody GuideDto guideDto) {
+		guideService.update(guideDto);
+	}
+}
