e2a26e73b681fbeeb05a703ce9460528fef46f38
TTaiJin <taeparis980110@gmail.com>
2025-02-03 23:33:06 +0900

    feat: JWT 예외 관련 처리 추가(#11)

--- backend/src/main/java/com/tripmarket/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/tripmarket/global/exception/GlobalExceptionHandler.java
@@ -8,10 +8,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("서버에서 오류가 발생했습니다: " + e.getMessage());
-    }
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleException(Exception e) {
+		return ResponseEntity
+			.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			.body("서버에서 오류가 발생했습니다: " + e.getMessage());
+	}
+
+	@ExceptionHandler(JwtAuthenticationException.class)
+	public ResponseEntity<ErrorResponse> handleException(JwtAuthenticationException e) {
+		ErrorResponse errorResponse = ErrorResponse.builder()
+			.status(e.getStatus())
+			.message(e.getMessage())
+			.build();
+
+		return ResponseEntity
+			.status(e.getStatus())
+			.body(errorResponse);
+	}
 }
