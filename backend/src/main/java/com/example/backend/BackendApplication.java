c244c9bc19e63085e1bb7a5127d6d9751badd4ee
이건희 <dnzp74@g.eulji.ac.kr>
2025-02-04 03:19:07 +0900

    feat: @EnableJpa 관련 설정 활성화(#9)

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -3,9 +3,13 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.example.backend", "com.tripmarket"})
+@EnableJpaRepositories(basePackages = {"com.tripmarket.domain"})
 @EntityScan(basePackages = {"com.tripmarket.domain"})
+@EnableJpaAuditing
 public class BackendApplication {
 
     public static void main(String[] args) {
