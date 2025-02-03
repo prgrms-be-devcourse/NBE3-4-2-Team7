b8c6ed964d22ac4205178cd9aa7863f8961064f7
TTaiJin <taeparis980110@gmail.com>
2025-02-04 00:55:13 +0900

    refactor: OAuth2UserInfo 주석 추가(#11)

--- backend/src/main/java/com/tripmarket/global/oauth2/userinfo/OAuth2UserInfo.java
+++ backend/src/main/java/com/tripmarket/global/oauth2/userinfo/OAuth2UserInfo.java
@@ -2,6 +2,9 @@ package com.tripmarket.global.oauth2.userinfo;
 
 import java.util.Map;
 
+/**
+ * 소셜 로그인 제공자별 유저 정보를 매핑하기 위한 추상 클래스
+ */
 public abstract class OAuth2UserInfo {
 	protected Map<String, Object> attributes;
 
@@ -9,11 +12,11 @@ public abstract class OAuth2UserInfo {
 		this.attributes = attributes;
 	}
 
-	public abstract String getId();
+	public abstract String getId(); // 소셜 식별자
 
-	public abstract String getEmail();
+	public abstract String getEmail(); // 이메일
 
-	public abstract String getName();
+	public abstract String getName(); // 이름
 
-	public abstract String getImageUrl();
+	public abstract String getImageUrl(); // 프로필 이미지
 }
