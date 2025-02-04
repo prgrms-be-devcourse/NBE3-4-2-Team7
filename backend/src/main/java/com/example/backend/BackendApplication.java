471a381433e9e7af98d77b28ee5a930d562a3db8
TTaiJin <taeparis980110@gmail.com>
2025-02-04 15:35:15 +0900

    feat: EnableJpaRepositories, EnableJpaAuditing 추가(#11)

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -3,13 +3,17 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.example.backend", "com.tripmarket"})
 @EntityScan(basePackages = {"com.tripmarket.domain"})
+@EnableJpaRepositories(basePackages = {"com.tripmarket.domain"})
+@EnableJpaAuditing
 public class BackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
 
 }
