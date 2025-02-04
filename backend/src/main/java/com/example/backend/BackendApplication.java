22673c577e2b0fd56a06250f67a82db7f42a6019
강정수 <emdtod12@naver.com>
2025-02-03 21:29:05 -0800

    feat : Application에서 repository랑 entity읽을수 있도록 변경(#7)

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -3,9 +3,11 @@ package com.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.example.backend", "com.tripmarket"})
 @EntityScan(basePackages = {"com.tripmarket.domain"})
+@EnableJpaRepositories(basePackages = "com.tripmarket.domain.chatting.repository")
 public class BackendApplication {
 
     public static void main(String[] args) {
