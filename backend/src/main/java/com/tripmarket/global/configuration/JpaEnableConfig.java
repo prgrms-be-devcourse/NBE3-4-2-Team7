d4032ef59d42bcd88a98bf05409fbfe09ffd7a92
ByeongGyu Jeon <cube9928@naver.com>
2025-02-04 11:20:36 +0900

    feat : mvc 테스트 용이성을 위한 JpaEnableConfig 추가

---
+++ backend/src/main/java/com/tripmarket/global/configuration/JpaEnableConfig.java
@@ -0,0 +1,11 @@
+package com.tripmarket.global.configuration;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = {"com.tripmarket.domain"})
+@EntityScan(basePackages = {"com.tripmarket.domain"})
+public class JpaEnableConfig {
+}
