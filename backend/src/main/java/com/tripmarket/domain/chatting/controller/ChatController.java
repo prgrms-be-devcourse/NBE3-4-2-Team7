323b25a25c76d4e532c476307ea7d7cb3e1157e8
강정수 <emdtod12@naver.com>
2025-02-03 21:37:13 -0800

    Feat : 메세지 보내는 메서드 작성(#7)

---
+++ backend/src/main/java/com/tripmarket/domain/chatting/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.tripmarket.domain.chatting.controller;
+
+import com.tripmarket.domain.chatting.dto.MessageDto;
+import com.tripmarket.domain.chatting.service.MessageService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class ChatController {
+
+	private final MessageService messageService;
+
+	@MessageMapping("chat.message.{roomId}")
+	public void message(@DestinationVariable String roomId, MessageDto messageDto) {
+		log.info("메세지 전달 완료 {}: {}", roomId, messageDto);
+		messageService.sendMessageToRoom(messageDto);
+	}
+}
