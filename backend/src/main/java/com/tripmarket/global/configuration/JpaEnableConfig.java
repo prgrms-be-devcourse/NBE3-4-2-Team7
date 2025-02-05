9f4e10a77c242cf7f7f32eeff31ad17ea7990c2f
강정수 <emdtod12@naver.com>
2025-02-05 00:11:14 -0800

    Feat : message부분 mongodb로 바꾸면서 scan제외하도록 변경(#7)

--- backend/src/main/java/com/tripmarket/global/configuration/JpaEnableConfig.java
+++ backend/src/main/java/com/tripmarket/global/configuration/JpaEnableConfig.java
@@ -1,12 +1,16 @@
 package com.tripmarket.global.configuration;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = {"com.tripmarket.domain"})
+@EnableJpaRepositories(basePackages = {"com.tripmarket.domain"}, excludeFilters = {
+	@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {com.tripmarket.domain.chatting.repository.MessageRepository.class})
+})
 @EntityScan(basePackages = {"com.tripmarket.domain"})
 @EnableJpaAuditing
 public class JpaEnableConfig {
