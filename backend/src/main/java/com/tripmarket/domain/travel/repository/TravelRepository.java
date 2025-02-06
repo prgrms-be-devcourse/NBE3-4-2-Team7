02565d05420bd965116404164da136b3fde9a12b
amormanet <makesense7899@gmail.com>
2025-02-06 23:49:21 +0900

    Init: 여행 레포지토리 코드 추가 (#4)
    - 레포지토리 커스텀에 있는 메소드 주석처리

--- backend/src/main/java/com/tripmarket/domain/travel/repository/TravelRepository.java
+++ backend/src/main/java/com/tripmarket/domain/travel/repository/TravelRepository.java
@@ -3,16 +3,9 @@ package com.tripmarket.domain.travel.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.tripmarket.domain.guide.entity.Guide;
-import com.tripmarket.domain.member.entity.Member;
 import com.tripmarket.domain.travel.entity.Travel;
 
 @Repository
 public interface TravelRepository extends JpaRepository<Travel, Long>, TravelRepositoryCustom {
 
-	default boolean hasMatchedTravel(Member user, Guide guide) {
-		return existsByUserAndGuideAndStatus(user, guide, Travel.Status.MATCHED);
-	}
-
-	boolean existsByUserAndGuideAndStatus(Member user, Guide guide, Travel.Status status);
 }
