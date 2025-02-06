a5262744fcec55f51eb7720ee396f0a578ecf8e4
강정수 <emdtod12@naver.com>
2025-02-05 18:48:26 -0800

    Feat : custom과 impl로 역할 분리(#7)

--- backend/src/main/java/com/tripmarket/domain/chatting/repository/chattingroom/ChattingRoomRepository.java
+++ backend/src/main/java/com/tripmarket/domain/chatting/repository/chattingroom/ChattingRoomRepository.java
@@ -1,10 +1,7 @@
-package com.tripmarket.domain.chatting.repository;
+package com.tripmarket.domain.chatting.repository.chattingroom;
 
 import com.tripmarket.domain.chatting.entity.ChattingRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
-public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, Long> {
-
-	Optional<ChattingRoom> findByUser1IdAndUser2Id(Long user1Id, Long User2Id);
+public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, Long>,CustomChattingRoomRepository {
 }
