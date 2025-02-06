1bb387101da45ef3cb7dd053c20453fa99ce6f1b
강정수 <emdtod12@naver.com>
2025-02-05 23:49:43 -0800

    Feat : 내 채팅방 목록 가져오기 개발(#7)

--- backend/src/main/java/com/tripmarket/domain/chatting/repository/chattingroom/ChattingRoomRepositoryImpl.java
+++ backend/src/main/java/com/tripmarket/domain/chatting/repository/chattingroom/ChattingRoomRepositoryImpl.java
@@ -1,7 +1,15 @@
 package com.tripmarket.domain.chatting.repository.chattingroom;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.tripmarket.domain.chatting.entity.ChattingRoom;
 import com.tripmarket.domain.chatting.entity.QChattingRoom;
 import com.tripmarket.domain.chatting.entity.QChattingRoomParticipant;
 
@@ -27,5 +35,33 @@ public class ChattingRoomRepositoryImpl implements CustomChattingRoomRepository 
 			.join(chattingRoom.participants, participant2)
 			.where(condition)
 			.fetchOne() != null;
+	}
+
+	@Override
+	public Page<ChattingRoom> findChattingRooms(String userEmail, String search, Pageable pageable) {
+		QChattingRoom chattingRoom = QChattingRoom.chattingRoom;
+		QChattingRoomParticipant participant = QChattingRoomParticipant.chattingRoomParticipant;
+
+		BooleanBuilder condition = new BooleanBuilder();
+		condition.and(participant.member.email.eq(userEmail));
+
+		if (search != null && !search.isEmpty()) {
+			condition.and(chattingRoom.participants.any().member.name.containsIgnoreCase(search));
+		}
+
+		List<ChattingRoom> chattingRooms = queryFactory.selectFrom(chattingRoom)
+			.join(chattingRoom.participants, participant)
+			.where(condition)
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
+			.fetch();
+
+		long total = queryFactory.selectFrom(chattingRoom)
+			.join(chattingRoom.participants, participant)
+			.where(condition)
+			.fetchCount();
+
+		// Page로 변환해서 반환
+		return new PageImpl<>(chattingRooms, pageable, total);
 	}
 }
--- backend/src/main/java/com/tripmarket/domain/chatting/repository/chattingroom/CustomChattingRoomRepository.java
+++ backend/src/main/java/com/tripmarket/domain/chatting/repository/chattingroom/CustomChattingRoomRepository.java
@@ -1,5 +1,11 @@
 package com.tripmarket.domain.chatting.repository.chattingroom;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.tripmarket.domain.chatting.entity.ChattingRoom;
+
 public interface CustomChattingRoomRepository {
 	boolean findRoomByParticipants(String email1, String email2);
+	Page<ChattingRoom> findChattingRooms(String userEmail, String searchName, Pageable pageable);
 }
