e8fbad980688fe3d5186f776f99ec4a83ffd2ccb
강정수 <emdtod12@naver.com>
2025-02-05 18:46:21 -0800

    Feat : 참여자 분리(#7)

---
+++ backend/src/main/java/com/tripmarket/domain/chatting/entity/ChattingRoomParticipant.java
@@ -0,0 +1,31 @@
+package com.tripmarket.domain.chatting.entity;
+
+import com.tripmarket.domain.member.entity.Member;
+import com.tripmarket.global.jpa.entity.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ChattingRoomParticipant extends BaseEntity {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "room_id")
+	private ChattingRoom chattingRoom;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+}
