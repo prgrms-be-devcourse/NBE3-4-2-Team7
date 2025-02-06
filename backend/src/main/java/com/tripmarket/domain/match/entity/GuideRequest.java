cf30b4ecca34e78eae0bef506e23f7baf67a5bb4
amormanet <makesense7899@gmail.com>
2025-02-06 23:20:55 +0900

    Feat : 가이드요청, 여행오퍼 엔티티에 베이스엔티티 상속 추가 (#4)

---
+++ backend/src/main/java/com/tripmarket/domain/match/entity/GuideRequest.java
@@ -0,0 +1,67 @@
+package com.tripmarket.domain.match.entity;
+
+import com.tripmarket.domain.guide.entity.Guide;
+import com.tripmarket.domain.member.entity.Member;
+import com.tripmarket.domain.travel.entity.Travel;
+import com.tripmarket.global.exception.CustomException;
+import com.tripmarket.global.exception.ErrorCode;
+import com.tripmarket.global.jpa.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GuideRequest extends BaseEntity {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private Member user; // 요청한 사용자
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "guide_id", nullable = false)
+	private Guide guide; // 요청받은 가이더
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "travel_id", nullable = false)
+	private Travel travel; // 여행 정보
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private RequestStatus status = RequestStatus.PENDING;
+
+	public void updateStatus(RequestStatus newStatus) {
+		if (this.status == RequestStatus.ACCEPTED || this.status == RequestStatus.REJECTED) {
+			throw new CustomException(ErrorCode.REQUEST_ALREADY_PROCESSED);
+		}
+
+		if (newStatus == RequestStatus.PENDING) {
+			throw new CustomException(ErrorCode.INVALID_REQUEST_STATUS);
+		}
+		this.status = newStatus;
+
+		if (newStatus == RequestStatus.ACCEPTED) {
+			this.travel.updateTravelStatus(Travel.Status.MATCHED);
+		}
+
+		if (newStatus == RequestStatus.REJECTED) {
+			this.travel.updateTravelStatus(Travel.Status.WAITING_FOR_MATCHING);
+		}
+	}
+
+	public enum RequestStatus {
+		PENDING, ACCEPTED, REJECTED
+	}
+}
---
+++ backend/src/main/java/com/tripmarket/domain/match/entity/TravelOffer.java
@@ -0,0 +1,65 @@
+package com.tripmarket.domain.match.entity;
+
+import com.tripmarket.domain.guide.entity.Guide;
+import com.tripmarket.domain.travel.entity.Travel;
+import com.tripmarket.global.exception.CustomException;
+import com.tripmarket.global.exception.ErrorCode;
+import com.tripmarket.global.jpa.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class TravelOffer extends BaseEntity {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "guide_id", nullable = false)
+	private Guide guide; // 요청을 보낸 가이더
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "travel_id", nullable = false)
+	private Travel travel; // 대상 여행 요청 글
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private RequestStatus status = RequestStatus.PENDING;
+
+	@Column(columnDefinition = "TEXT")
+	private String message; // 가이더의 제안 메시지 (선택 사항)
+
+	public void updateStatus(RequestStatus newStatus) {
+		if (this.status == RequestStatus.ACCEPTED || this.status == RequestStatus.REJECTED) {
+			throw new CustomException(ErrorCode.REQUEST_ALREADY_PROCESSED);
+		}
+
+		if (newStatus == RequestStatus.PENDING) {
+			throw new CustomException(ErrorCode.INVALID_REQUEST_STATUS);
+		}
+		this.status = newStatus;
+
+		if (newStatus == RequestStatus.ACCEPTED) {
+			this.travel.updateTravelStatus(Travel.Status.MATCHED);
+		}
+
+		if (newStatus == RequestStatus.REJECTED) {
+			this.travel.updateTravelStatus(Travel.Status.WAITING_FOR_MATCHING);
+		}
+	}
+
+	public enum RequestStatus {
+		PENDING, ACCEPTED, REJECTED
+	}
+}
