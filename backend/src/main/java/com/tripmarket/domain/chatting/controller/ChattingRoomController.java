49d024a895bed4e8fc4793cce2c7b1057d302dea
강정수 <emdtod12@naver.com>
2025-02-05 18:54:56 -0800

    Feat : 로그인된 유저 정보 받아오게 변경(#7)

--- backend/src/main/java/com/tripmarket/domain/chatting/controller/ChattingRoomController.java
+++ backend/src/main/java/com/tripmarket/domain/chatting/controller/ChattingRoomController.java
@@ -1,6 +1,7 @@
 package com.tripmarket.domain.chatting.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tripmarket.domain.chatting.dto.ChattingRoomRequestDto;
 import com.tripmarket.domain.chatting.service.ChattingRoomService;
+import com.tripmarket.global.oauth2.CustomOAuth2User;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -15,7 +17,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/chatting-room")
 @Tag(name = "채팅룸", description = "채팅룸 컨트롤러")
@@ -31,8 +35,10 @@ public class ChattingRoomController {
 		@ApiResponse(responseCode = "404", description = "존재하지 않는 유저입니다."),
 		@ApiResponse(responseCode = "409", description = "채팅방이 이미 존재"),
 		@ApiResponse(responseCode = "500", description = "서버 오류")})
-	public ResponseEntity<?> createChatRoom(@RequestBody @Valid ChattingRoomRequestDto request) {
-		chattingRoomService.create(request);
+	public ResponseEntity<?> createChatRoom(@RequestBody @Valid ChattingRoomRequestDto request,
+		@AuthenticationPrincipal CustomOAuth2User user) {
+		log.info("user : {}",user.getEmail());
+		chattingRoomService.create(user.getEmail(),request.getTarget());
 		return ResponseEntity.ok(null);
 	}
 }
