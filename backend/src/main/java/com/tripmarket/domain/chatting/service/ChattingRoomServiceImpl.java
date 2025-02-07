b4f343983c8c9c86ab473ff9e356485936682705
강정수 <emdtod12@naver.com>
2025-02-06 21:45:04 -0800

    Refactor : 메서드 분리(#7)

--- backend/src/main/java/com/tripmarket/domain/chatting/service/ChattingRoomServiceImpl.java
+++ backend/src/main/java/com/tripmarket/domain/chatting/service/ChattingRoomServiceImpl.java
@@ -36,72 +36,87 @@ public class ChattingRoomServiceImpl implements ChattingRoomService {
 	private final MessageRepository messageRepository;
 
 	@Transactional
-	public void create(String userEmail, String targetEmail) {
-		Member user = validateMember(userEmail);
-		Member target = validateMember(targetEmail);
+	public void create(String userEmail, String receiverEmail) {
+		Member user = findMember(userEmail);
+		Member receiver = findMember(receiverEmail);
+		validateChattingRoom(userEmail, receiverEmail);
 
-		if (chattingRoomRepository.findRoomByParticipants(userEmail, targetEmail)) {
-			throw new CustomException(ErrorCode.DUPLICATE_CHAT_ROOM);
-		}
-
-		ChattingRoom chattingRoom = ChattingRoom.builder()
-			.participants(new HashSet<>())
-			.build();
-		addParticipants(chattingRoom, user, target);
+		ChattingRoom chattingRoom = addParticipants(user, receiver);
 
 		ChattingRoom save = chattingRoomRepository.save(chattingRoom);
-		log.info("채팅방이 생성되었습니다. 채팅방 참여자: {}", String.join(", ", save.getParticipantEmails()));
+		log.debug("채팅방이 생성되었습니다. 채팅방 참여자: {}", String.join(", ", save.getParticipantEmails()));
 	}
 
 	@Override
 	public Page<ChattingRoomsResponseDto> findChattingRooms(String userEmail, String search, Pageable pageable) {
+		Page<ChattingRoom> chattingRoomsPage = findChattingRoomsPage(userEmail, search, pageable);
 
-		Page<ChattingRoom> chattingRoomsPage = chattingRoomRepository.findChattingRooms(userEmail, search, pageable);
-		log.info("채팅방 목록을 받았습니다. 총 채팅방 수: {}", chattingRoomsPage.getTotalElements());
-
-		List<String> roomIds = chattingRoomsPage.getContent().stream()
-			.map(chattingRoom -> chattingRoom.getId().toString())
-			.toList();
-		log.info("채팅방 id들을 찾았습니다.");
-
+		List<String> roomIds = findRoomIds(chattingRoomsPage);
 		List<Message> latestMessages = messageRepository.findLatestMessages(roomIds);
-		log.info("최신 메시지 조회 - 메시지 개수: {}", latestMessages.size());
+		Map<String, Message> messageMap = getMessageMap(latestMessages);
 
-		Map<String, Message> messageMap = latestMessages.stream()
-			.collect((Collectors.toMap(Message::getRoomId, message -> message)));
-
-		//채팅방 목록 받아오는 곳
-		List<ChattingRoomsResponseDto> chattingRoomsResponse = chattingRoomsPage.getContent().stream()
-			.map(chattingRoom -> {
-				Long roomId = chattingRoom.getId();
-				Message lastMessage = messageMap.get(roomId.toString());
-
-				// 상대 정보 받아오기
-				Member target = chattingRoom.getParticipants().stream()
-					.filter(participant -> !participant.getMember().getEmail().equals(userEmail))
-					.findFirst()
-					.orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND))
-					.getMember();
-				return ChattingRoomsResponseDto.of(roomId, target, lastMessage);
-			})
-			.sorted(Comparator.comparing(ChattingRoomsResponseDto::getLastMessageTime,
-				Comparator.nullsFirst(Comparator.reverseOrder())))
-			.toList();
+		List<ChattingRoomsResponseDto> chattingRoomsResponse = getChattingRoomsResponse(userEmail, chattingRoomsPage, messageMap);
 
 		return new PageImpl<>(chattingRoomsResponse, pageable, chattingRoomsPage.getTotalElements());
 	}
 
-	private static void addParticipants(ChattingRoom chattingRoom, Member user, Member target) {
-		Set<ChattingRoomParticipant> participants = new HashSet<>();
-		participants.add(new ChattingRoomParticipant(chattingRoom, user));
-		participants.add(new ChattingRoomParticipant(chattingRoom, target));
-		chattingRoom.getParticipants().addAll(participants);
+	private static List<ChattingRoomsResponseDto> getChattingRoomsResponse(String userEmail, Page<ChattingRoom> chattingRoomsPage,
+		Map<String, Message> messageMap) {
+		return chattingRoomsPage.getContent().stream()
+			.map(chattingRoom -> {
+				Long roomId = chattingRoom.getId();
+				Message lastMessage = messageMap.get(roomId.toString());
+				Member receiver = getReceiver(userEmail, chattingRoom);
+				return ChattingRoomsResponseDto.of(roomId, receiver, lastMessage);
+			})
+			.sorted(Comparator.comparing(ChattingRoomsResponseDto::lastMessageTime,
+				Comparator.nullsFirst(Comparator.reverseOrder())))
+			.toList();
 	}
 
-	private Member validateMember(String email) {
+	private static Member getReceiver(String userEmail, ChattingRoom chattingRoom) {
+		return chattingRoom.getParticipants().stream()
+			.filter(participant -> !participant.getMember().getEmail().equals(userEmail))
+			.findFirst()
+			.orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND))
+			.getMember();
+	}
+
+	private static Map<String, Message> getMessageMap(List<Message> latestMessages) {
+		return latestMessages.stream().collect(Collectors.toMap(Message::getRoomId, message -> message));
+	}
+
+	private static List<String> findRoomIds(Page<ChattingRoom> chattingRoomsPage) {
+		return chattingRoomsPage.getContent().stream()
+			.map(chattingRoom -> chattingRoom.getId().toString())
+			.toList();
+	}
+
+	private Page<ChattingRoom> findChattingRoomsPage(String userEmail, String search, Pageable pageable) {
+		return chattingRoomRepository.findChattingRooms(userEmail, search, pageable);
+	}
+
+	private ChattingRoom addParticipants(Member user, Member receiver) {
+		ChattingRoom chattingRoom = ChattingRoom.builder()
+			.participants(new HashSet<>())
+			.build();
+		Set<ChattingRoomParticipant> participants = new HashSet<>();
+		participants.add(new ChattingRoomParticipant(chattingRoom, user));
+		participants.add(new ChattingRoomParticipant(chattingRoom, receiver));
+		chattingRoom.getParticipants().addAll(participants);
+		return chattingRoom;
+	}
+
+	private void validateChattingRoom(String userEmail, String targetEmail) {
+		if (chattingRoomRepository.findRoomByParticipants(userEmail, targetEmail)) {
+			throw new CustomException(ErrorCode.DUPLICATE_CHAT_ROOM);
+		}
+	}
+
+	private Member findMember(String email) {
 		Member member = memberRepository.findByEmail(email)
 			.orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
-		log.info("유저를 찾았습니다. email ={}", member.getEmail());
+		log.debug("유저를 찾았습니다. email ={}", member.getEmail());
 		return member;
 	}
 }
