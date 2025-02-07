bb11f39e40db98726617481c2c89ebd27efafce2
이건희 <dnzp74@g.eulji.ac.kr>
2025-02-07 14:11:26 +0900

    refactor: 매칭 관련 api 인증 객체에서 email 받아오도록 수정(#22)

--- backend/src/main/java/com/tripmarket/domain/match/controller/GuideRequestController.java
+++ backend/src/main/java/com/tripmarket/domain/match/controller/GuideRequestController.java
@@ -2,6 +2,7 @@ package com.tripmarket.domain.match.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tripmarket.domain.match.dto.GuideRequestCreate;
+import com.tripmarket.domain.match.dto.request.GuideRequestCreate;
 import com.tripmarket.domain.match.entity.GuideRequest;
 import com.tripmarket.domain.match.service.GuideRequestService;
+import com.tripmarket.global.oauth2.CustomOAuth2User;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -32,11 +33,11 @@ public class GuideRequestController {
 	@Operation(summary = "사용자가 가이더에게 여행 요청을 보냄")
 	@PostMapping("/{guideId}")
 	public ResponseEntity<String> createGuideRequest(
-		@PathVariable Long guideId,
-		@Parameter(description = "사용자 ID (임시, 추후 인증 객체로 변경 예정)") @RequestParam Long userId,
+		@PathVariable(name = "guideId") Long guideId,
+		@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
 		@RequestBody @Valid GuideRequestCreate requestDto
 	) {
-		guideRequestService.createGuideRequest(userId, guideId, requestDto);
+		guideRequestService.createGuideRequest(customOAuth2User.getEmail(), guideId, requestDto);
 		return ResponseEntity.status(HttpStatus.CREATED).body("가이더에게 매칭 요청이 완료되었습니다.");
 	}
 
@@ -44,9 +45,9 @@ public class GuideRequestController {
 		description = "사용자의 매칭 요청을 보고 가이더가 이를 수락하거나 거절하는 API입니다.")
 	@PatchMapping("{requestId}/match")
 	public ResponseEntity<String> matchGuideRequest(
-		@PathVariable Long requestId,
-		@RequestParam Long guideId,
-		@RequestParam GuideRequest.RequestStatus status
+		@PathVariable(name = "requestId") Long requestId,
+		@RequestParam(name = "guideId") Long guideId,
+		@RequestParam(name = "status") GuideRequest.RequestStatus status
 	) {
 		guideRequestService.matchGuideRequest(requestId, guideId, status);
 		return ResponseEntity.ok("요청 상태가 업데이트되었습니다.");
