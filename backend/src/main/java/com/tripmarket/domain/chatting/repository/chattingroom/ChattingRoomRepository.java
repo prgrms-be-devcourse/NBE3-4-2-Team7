f994ba658ec04e4d47606edfd26442ee4cc9ffad
강정수 <emdtod12@naver.com>
2025-02-07 04:23:09 -0800

    Refactor : JpaRepository에서 id 타입 String으로 변경(#7)

--- backend/src/main/java/com/tripmarket/domain/chatting/repository/chattingroom/ChattingRoomRepository.java
+++ backend/src/main/java/com/tripmarket/domain/chatting/repository/chattingroom/ChattingRoomRepository.java
@@ -1,7 +1,8 @@
 package com.tripmarket.domain.chatting.repository.chattingroom;
 
 import com.tripmarket.domain.chatting.entity.ChattingRoom;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, Long>,CustomChattingRoomRepository {
+public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, String>, CustomChattingRoomRepository {
 }
