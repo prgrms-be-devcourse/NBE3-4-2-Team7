d5ae35c5f627bf4be383fc4e1a12d45c66d202ec
강정수 <emdtod12@naver.com>
2025-02-09 16:27:47 -0800

    refcator : log단위 디버그로 수정(#7)

--- backend/src/main/java/com/tripmarket/domain/chatting/controller/WebSocketEventController.java
+++ backend/src/main/java/com/tripmarket/domain/chatting/controller/WebSocketEventController.java
@@ -1,8 +1,6 @@
 package com.tripmarket.domain.chatting.controller;
 
 import org.springframework.context.event.EventListener;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
@@ -17,18 +15,12 @@ public class WebSocketEventController {
 	@EventListener
 	public void handleWebSocketConnectListener(SessionConnectEvent event) {
 		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-		log.info("WebSocket 연결: {}", headerAccessor.getSessionId());
+		log.debug("WebSocket 연결: {}", headerAccessor.getSessionId());
 	}
 
 	@EventListener
 	public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
 		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-		log.info("WebSocket 연결 해제: {}", headerAccessor.getSessionId());
+		log.debug("WebSocket 연결 해제: {}", headerAccessor.getSessionId());
 	}
-
-	@MessageMapping("/**")
-	public void debugMessages(Message<?> message) {
-		System.out.println("DEBUG: Received raw message - " + message);
-	}
-
 }
