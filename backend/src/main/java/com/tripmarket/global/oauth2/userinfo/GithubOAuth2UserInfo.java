cfdf8f97eaeec1c2c5a15d857b30132abe89725b
Okman-0920 <nohoksun94@gmail.com>
2025-02-10 22:12:49 +0900

    refactor: 깃 로그인 오류 해결(unique 해제)

--- backend/src/main/java/com/tripmarket/global/oauth2/userinfo/GithubOAuth2UserInfo.java
+++ backend/src/main/java/com/tripmarket/global/oauth2/userinfo/GithubOAuth2UserInfo.java
@@ -1,12 +1,16 @@
 package com.tripmarket.global.oauth2.userinfo;
 
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 깃허브 OAuth2 사용자 정보를 처리하는 구현체
  * 깃허브 API가 제공하는 사용자 정보 구조에 맞춰 정보를 추출
  */
 public class GithubOAuth2UserInfo extends OAuth2UserInfo {
+
+	private static final Logger log = LoggerFactory.getLogger(GithubOAuth2UserInfo.class);
 
 	public GithubOAuth2UserInfo(Map<String, Object> attributes) {
 		super(attributes);
@@ -19,12 +23,15 @@ public class GithubOAuth2UserInfo extends OAuth2UserInfo {
 
 	@Override
 	public String getEmail() {
-		return (String) attributes.get("email");
+		String email = (String) attributes.get("email");
+		log.debug("GitHub email: {}", email);
+		return email;
 	}
 
 	@Override
 	public String getName() {
-		return (String) attributes.get("name");
+		String name = (String) attributes.get("name");
+		return name != null ? name : (String) attributes.get("login");
 	}
 
 	@Override
