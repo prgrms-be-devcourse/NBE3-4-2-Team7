9027be88321205bebc8283abea1906edf6a3e63e
TTaiJin <taeparis980110@gmail.com>
2025-02-10 15:46:30 +0900

    refactor: 소셜 로그인에 따른 userInfo 객체 생성(#25)

--- backend/src/main/java/com/tripmarket/global/oauth2/service/CustomOAuth2UserService.java
+++ backend/src/main/java/com/tripmarket/global/oauth2/service/CustomOAuth2UserService.java
@@ -16,6 +16,7 @@ import com.tripmarket.domain.member.entity.Member;
 import com.tripmarket.domain.member.entity.Provider;
 import com.tripmarket.domain.member.repository.MemberRepository;
 import com.tripmarket.global.oauth2.CustomOAuth2User;
+import com.tripmarket.global.oauth2.userinfo.GoogleOAuth2UserInfo;
 import com.tripmarket.global.oauth2.userinfo.KakaoOAuth2UserInfo;
 import com.tripmarket.global.oauth2.userinfo.OAuth2UserInfo;
 
@@ -55,7 +56,11 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 			.getUserNameAttributeName();
 
 		// OAuth2UserInfo 객체 생성
-		OAuth2UserInfo userInfo = new KakaoOAuth2UserInfo(oAuth2User.getAttributes());
+		OAuth2UserInfo userInfo = switch (registrationId.toLowerCase()) {
+			case "kakao" -> new KakaoOAuth2UserInfo(oAuth2User.getAttributes());
+			case "google" -> new GoogleOAuth2UserInfo(oAuth2User.getAttributes());
+			default -> throw new OAuth2AuthenticationException("지원하지 않는 소셜 로그인입니다: " + registrationId);
+		};
 
 		// 유저 정보 저장 또는 업데이트
 		Member member = saveOrUpdate(userInfo, registrationId);
