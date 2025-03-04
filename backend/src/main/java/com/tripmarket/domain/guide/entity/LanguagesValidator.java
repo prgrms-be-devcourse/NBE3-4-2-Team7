c3e6e157cbc868f5583799789e1878d8af9770b7
ByeongGyu Jeon <cube9928@naver.com>
2025-03-04 17:29:51 +0900

    refactor: 언어 입력, 출력, 검증 로직 변경
    언어 입력 방식 한국어 쓸 수 있게 변경 (ex. "한국어, 영어")
    여러 입력 값 케이스에 대한 테스트 추가

--- backend/src/main/java/com/tripmarket/domain/guide/entity/LanguagesValidator.java
+++ backend/src/main/java/com/tripmarket/domain/guide/entity/LanguagesValidator.java
@@ -1,17 +1,27 @@
 package com.tripmarket.domain.guide.entity;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 @Component
 public class LanguagesValidator implements ConstraintValidator<ValidLanguages, String> {
+	private static final Set<String> ISO_DISPLAY_LANGUAGE_SET = new HashSet<>();
+
+	@PostConstruct
+	public void init() {
+		// Locale 데이터 초기화
+		for (String language : Locale.getISOLanguages()) {
+			Locale locale = new Locale(language);
+			ISO_DISPLAY_LANGUAGE_SET.add(locale.getDisplayLanguage());
+		}
+	}
 
 	@Override
 	public boolean isValid(String languages, ConstraintValidatorContext context) {
@@ -19,13 +29,12 @@ public class LanguagesValidator implements ConstraintValidator<ValidLanguages, S
 			return false;
 		}
 
-		Set<String> ISOLanguagesSet = new HashSet<>(Arrays.asList(Locale.getISOLanguages()));
-
 		String[] languageArray = languages.split(",");
 		for (String language : languageArray) {
+
 			language = language.trim();
 
-			if (!ISOLanguagesSet.contains(language)) {
+			if (!ISO_DISPLAY_LANGUAGE_SET.contains(language)) {
 				return false;
 			}
 		}
