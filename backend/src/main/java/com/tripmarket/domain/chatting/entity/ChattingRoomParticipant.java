fe69c1a58d5c9947b935e2a276917dbe1e6ff367
강정수 <emdtod12@naver.com>
2025-03-04 22:25:44 -0800

    feat : memberid 인덱싱 처리

--- backend/src/main/java/com/tripmarket/domain/chatting/entity/ChattingRoomParticipant.java
+++ backend/src/main/java/com/tripmarket/domain/chatting/entity/ChattingRoomParticipant.java
@@ -8,8 +8,10 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +20,9 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Table(name = "chatting_room_participant", indexes = {
+	@Index(name = "idx_participant_email", columnList = "member_id")
+})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
