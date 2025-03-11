cde9cfe3b6c8f9ef95f24c0d6b21f15eb7a74fb4
TTaiJin <taeparis980110@gmail.com>
2025-03-11 09:46:58 +0900

    refactor: MemberRepository 코틀린 마이그레이션(#64)

--- backend/src/main/java/com/tripmarket/domain/member/repository/MemberRepository.kt
+++ backend/src/main/java/com/tripmarket/domain/member/repository/MemberRepository.kt
@@ -1,16 +1,14 @@
-package com.tripmarket.domain.member.repository;
+package com.tripmarket.domain.member.repository
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.tripmarket.domain.member.entity.Member;
-import com.tripmarket.domain.member.entity.Provider;
+import com.tripmarket.domain.member.entity.Member
+import com.tripmarket.domain.member.entity.Provider
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-	Optional<Member> findByProviderAndProviderId(Provider provider, String providerId);
+interface MemberRepository : JpaRepository<Member, Long> {
+    fun findByProviderAndProviderId(provider: Provider, providerId: String): Optional<Member>
 
-	Optional<Member> findByEmail(String email);
+    fun findByEmail(email: String): Optional<Member>
 }
