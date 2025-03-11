202c160449312faa8186890db9e6fcc8ab629090
ByeongGyu Jeon <cube9928@naver.com>
2025-03-11 17:39:54 +0900

    refactor: 자바 코드 주석 제거

--- backend/src/main/java/com/tripmarket/domain/guide/entity/LanguagesValidator.kt
+++ backend/src/main/java/com/tripmarket/domain/guide/entity/LanguagesValidator.kt
@@ -1,37 +1,5 @@
 package com.tripmarket.domain.guide.entity;
 
-// @Component
-// public class LanguagesValidator implements ConstraintValidator<ValidLanguages, String> {
-// 	private static final Set<String> ISO_DISPLAY_LANGUAGE_SET = new HashSet<>();
-//
-// 	@PostConstruct
-// 	public void init() {
-// 		// Locale 데이터 초기화
-// 		for (String language : Locale.getISOLanguages()) {
-// 			Locale locale = new Locale(language);
-// 			ISO_DISPLAY_LANGUAGE_SET.add(locale.getDisplayLanguage());
-// 		}
-// 	}
-//
-// 	@Override
-// 	public boolean isValid(String languages, ConstraintValidatorContext context) {
-// 		if (languages == null) {
-// 			return false;
-// 		}
-//
-// 		String[] languageArray = languages.split(",");
-// 		for (String language : languageArray) {
-//
-// 			language = language.trim();
-//
-// 			if (!ISO_DISPLAY_LANGUAGE_SET.contains(language)) {
-// 				return false;
-// 			}
-// 		}
-//
-// 		return true;
-// 	}
-// }
 
 import jakarta.annotation.PostConstruct
 import jakarta.validation.ConstraintValidator
