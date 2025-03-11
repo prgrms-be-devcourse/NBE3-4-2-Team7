c8d29af9f79ecfbbda487cf7a2bddd646716d5ce
TTaiJin <taeparis980110@gmail.com>
2025-03-11 09:32:58 +0900

    refactor: Provider 코틀린 마이그레이션(#64)

--- backend/src/main/java/com/tripmarket/domain/member/entity/Provider.kt
+++ backend/src/main/java/com/tripmarket/domain/member/entity/Provider.kt
@@ -1,9 +1,8 @@
-package com.tripmarket.domain.member.entity;
+package com.tripmarket.domain.member.entity
 
-public enum Provider {
-	LOCAL, // 일반 회원가입용 추가
-	KAKAO,
-	GOOGLE,
-	GITHUB
-	// 추후 다른 소셜 로그인 추가
+enum class Provider {
+    LOCAL,  // 일반 회원가입용 추가
+    KAKAO,
+    GOOGLE,
+    GITHUB // 추후 다른 소셜 로그인 추가
 }
